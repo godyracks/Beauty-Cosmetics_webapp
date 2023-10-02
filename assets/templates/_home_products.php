d91f010597cc562e3487b19152caf19c1d9c8a83
GodyRacks <godfreymatagaro@gmail.com>
2023-10-02 12:41:08 +0300

    loop through product table

--- assets/templates/_home_products.php
+++ assets/templates/_home_products.php
@@ -1,72 +1,39 @@
- <!-- Product Cards -->
-	       
- <section class="products">
-		<h2 style="background-color: #fff;">Lace Wigs</h2>
-		<div class="all-products">
-			<div class="product">
-        <div class="product-image-container">
-        <a href="../details" style="background-color: white;">
-          <img src="../assets/img/model-1.png">
-          <div class="product-icons">
-            <a href="../wishlist"><i class='bx bx-heart'></i></a>
-            <!-- <i class='bx bx-cart-alt'></i> Cart icon for adding to cart -->
-          </div>
-            </a>
+<?php
+include_once('../assets/setup/db.php');
+
+// Assuming you are using MySQLi for database connection
+$sql = "SELECT p.*, i.image_url 
+        FROM products p
+        JOIN product_images i ON p.product_id = i.product_id";
+
+$result = $conn->query($sql);
+
+// Check for query execution success
+if (!$result) {
+    die("Query failed: " . $conn->error);
+}
+
+// Fetch only one row (assuming you want to display one product card)
+$row = $result->fetch_assoc();
+?>
+
+<section class="products">
+    <h2 style="background-color: #fff;">Lace Wigs</h2>
+    <div class="all-products">
+        <div class="product">
+            <div class="product-image-container">
+                <a href="../details" style="background-color: white;">
+                    <img src="../uploads/<?php echo $row['image_url']; ?>">
+                    <div class="product-icons">
+                        <a href="../wishlist"><i class='bx bx-heart'></i></a>
+                    </div>
+                </a>
+            </div>
+            <div class="product-info">
+                <h4 class="product-title"><?php echo $row['product_name']; ?></h4>
+                <p class="product-price">KES <?php echo number_format($row['price'], 2); ?></p>
+                <a class="product-btn" href="#">Buy Now</a>
+            </div>
         </div>
-        <div class="product-info">
-          <h4 class="product-title">HD Brazilian Lace Wigs</h4>
-          <p class="product-price">KES 1129</p>
-          <a class="product-btn" href="#">Buy Now</a>
-        </div>
-      </div>
-			<div class="product">
-        <div class="product-image-container">
-        <a href="../details" style="background-color: white;">
-          <img src="../assets/img/wig-cut.png">
-          <div class="product-icons">
-            <i class='bx bx-heart'></i>
-            <!-- <i class='bx bx-cart-alt'></i> Cart icon for adding to cart -->
-          </div>
-          </a>
-        </div>
-        <div class="product-info">
-          <h4 class="product-title">HD Brazilian Lace Wigs</h4>
-          <p class="product-price">KES 1129</p>
-          <a class="product-btn" href="#">Buy Now</a>
-        </div>
-      </div>
-    
-		
-      <div class="product">
-        <div class="product-image-container">
-        <a href="../details" style="background-color: white;">
-          <img src="../assets/img/curl.png">
-          <div class="product-icons">
-            <i class='bx bx-heart'></i>
-            <!-- <i class='bx bx-cart-alt'></i> Cart icon for adding to cart -->
-          </div>
-            </a>
-        </div>
-        <div class="product-info">
-          <h4 class="product-title">HD Brazilian Lace Wigs</h4>
-          <p class="product-price">KES 1129</p>
-          <a class="product-btn" href="#">Buy Now</a>
-        </div>
-      </div>
-      <div class="product">
-        <div class="product-image-container">
-          <img src="../assets/img/curl-wig.png">
-          <div class="product-icons">
-            <i class='bx bx-heart'></i>
-            <!-- <i class='bx bx-cart-alt'></i> Cart icon for adding to cart -->
-          </div>
-        </div>
-        <div class="product-info">
-          <h4 class="product-title">HD Brazilian Lace Wigs</h4>
-          <p class="product-price">KES 1129</p>
-          <a class="product-btn" href="#">Buy Now</a>
-        </div>
-      </div>
-		</div>
-	</section>
- <!-- /Product Cards -->
+    </div>
+</section>
